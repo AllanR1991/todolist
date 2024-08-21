e1f67a24c0eef4c3e7a96f040108b034a50be4ce
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-08-20 23:03:17 -0300

    inputSearch

--- src/components/InputSearch/InputSearch.js
+++ src/components/InputSearch/InputSearch.js
@@ -1,9 +1,12 @@
 import './style.css';
 
+
+
 export function InputSearch() {
     return(
-        <input className='input__search' type="text" placeholder="Procurar tarefa">
-
-        </input>
+        <div className='input__container'>
+            <input className='input__search' type="text" placeholder="Procurar tarefa"/>
+            <img className='input__img' src={require('./search2.png')} alt='adsdas'/>
+        </div>
     );
 }
